60fcd4c071abce8304b148ee405c96775c876698
Mije <serviciotecnico.mc.tdf@gmail.com>
2021-02-13 21:32:47 -0300

    footer

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,19 @@
   		toTop.classList.remove("active");
   	}
   })
+
+  jQuery('document').ready(function($){
+
+    var menuBtn= $('.menu-icon'),
+        menu=$('.navigation ul');
+
+    menuBtn.click(function(){
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        }else{
+            menu.addClass('show');
+        }
+
+    });
+
+});
